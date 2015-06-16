aa6122e627ba2587fc7b532bd1c8d02fd93b6f23
Catherine <cmedlock@mit.edu>
2015-06-16 14:18:49 +0200

    Second commit

--- ElectronNtupler/test/test.C
+++ ElectronNtupler/test/test.C
@@ -17,7 +17,7 @@
 #include "TH1F.h"
 #endif
 
-void test(){
+void test_sendToIlya(){
 
   TFile *f = new TFile("electron_ntuple.root");
   TTree *electronTree_ = (TTree*) f->FindObjectAny("ElectronTree");
@@ -88,37 +88,18 @@ void test(){
   electronTree_->SetBranchAddress("isTrue"    , &isTrue_);
 
   Int_t p=0;
-  TProfile *prof = new TProfile("prof", "prof", 9,10,100);
-  TProfile *prof_check = new TProfile("prof_check", "prof_check", 9,10,100);
+  TProfile *profID_pt = new TProfile("profID_pt", "profID_pt", 9,10,100);
+  TProfile *profCuts_pt = new TProfile("profCuts_pt", "profCuts_pt", 9,10,100);
+  TProfile *profID_eta = new TProfile("profID_eta", "profID_eta", 10,-2.5,2.5);
+  TProfile *profCuts_eta = new TProfile("profCuts_eta", "profCuts_eta", 10,-2.5,2.5);
 
-  TH1F* hrelIsoWithEA = new TH1F("hrelIsoWithEA","hrelIsoWithEA",50,0,0.2);
-  TH1F* hrelIsoWithEA_agree = new TH1F("hrelIsoWithEA_agree","hrelIsoWithEA_agree",50,0,0.2);
-  TH1F* hdEtaIn = new TH1F("hdEtaIn","hdEtaIn",50,-0.2,0.2);
-  TH1F* hdEtaIn_agree = new TH1F("hdEtaIn_agree","hdEtaIn_agree",50,-0.2,0.2);
-  TH1F* hdPhiIn = new TH1F("hdPhiIn","hdPhiIn",50,-0.3,0.3);
-  TH1F* hdPhiIn_agree = new TH1F("hdPhiIn_agree","hdPhiIn_agree",50,-0.3,0.3);
-  TH1F* hsieie = new TH1F("hsieie","hsieie",50,0,0.1);
-  TH1F* hsieie_agree = new TH1F("hsieie_agree","hsieie_agree",50,0,0.1);
-  TH1F* hhOverE = new TH1F("hhOverE","hhOverE",50,0,0.4);
-  TH1F* hhOverE_agree = new TH1F("hhOverE_agree","hhOverE_agree",50,0,0.4);
-  TH1F* hd0 = new TH1F("hd0","hd0",50,-0.4,0.4);
-  TH1F* hd0_agree = new TH1F("hd0_agree","hd0_agree",50,-0.4,0.4);
-  TH1F* hdz = new TH1F("hdz","hdz",50,-0.4,0.4);
-  TH1F* hdz_agree = new TH1F("hdz_agree","hdz_agree",50,-0.4,0.4);
-  TH1F* hooEmooP = new TH1F("hooEmooP","hooEmooP",50,0,0.3);
-  TH1F* hooEmooP_agree = new TH1F("hooEmooP_agree","hooEmooP_agree",50,0,0.3);
-  TH1F* hmissingHits = new TH1F("hmissingHits","hmissingHits",3,0,3);
-  TH1F* hmissingHits_agree = new TH1F("hmissingHits_agree","hmissingHits_agree",3,0,3);
-  TH1F* hconversion = new TH1F("hconversion","hconversion",3,0,3);
-  TH1F* hconversion_agree = new TH1F("hconversion_agree","hconversion_agree",3,0,3);
-
-  int trueisone = 0;
-  int trueiszero = 0;
+  int IDisone = 0;
+  int IDiszero = 0;
   for (Int_t i=0; i<electronTree_->GetEntries(); i++) {
     //for (Int_t i=0; i<10; i++) {
     electronTree_->GetEntry(i);
 
-    int passVetoId_check = 1;
+    int passVetoId_cuts = 1;
 
     double ea;
     if(0.0000 <= fabs(etaSC_) && fabs(etaSC_) < 0.8000) ea = 0.1013;
@@ -131,131 +112,57 @@ void test(){
 
     // barrel
     if(fabs(etaSC_)<=1.479) {
-      if(dEtaIn_ >= 0.013625) passVetoId_check = 0;
-      if(dPhiIn_ >= 0.230374) passVetoId_check = 0;
-      if(full5x5_sigmaIetaIeta_ >= 0.011586) passVetoId_check = 0;
-      if(hOverE_ >= 0.181130) passVetoId_check = 0;
-      if(d0_ >= 0.094095) passVetoId_check = 0;
-      if(dz_ >= 0.713070) passVetoId_check = 0;
-      if(ooEmooP_ >= 0.295751) passVetoId_check = 0;
-      if(relIsoWithEA >= 0.158721) passVetoId_check = 0;
-      if(expectedMissingInnerHits_>2) passVetoId_check = 0;
-      if(!passConversionVeto_) passVetoId_check = 0;
+      if(dEtaIn_ >= 0.013625) passVetoId_cuts = 0;
+      if(dPhiIn_ >= 0.230374) passVetoId_cuts = 0;
+      if(full5x5_sigmaIetaIeta_ >= 0.011586) passVetoId_cuts = 0;
+      if(hOverE_ >= 0.181130) passVetoId_cuts = 0;
+      if(d0_ >= 0.094095) passVetoId_cuts = 0;
+      if(dz_ >= 0.713070) passVetoId_cuts = 0;
+      if(ooEmooP_ >= 0.295751) passVetoId_cuts = 0;
+      if(relIsoWithEA >= 0.158721) passVetoId_cuts = 0;
+      if(expectedMissingInnerHits_>2) passVetoId_cuts = 0;
+      if(!passConversionVeto_) passVetoId_cuts = 0;
     } else if(1.479 < fabs(etaSC_) && fabs(etaSC_) < 2.5) { // endcap
-      if(dEtaIn_ >= 0.011932) passVetoId_check = 0;
-      if(dPhiIn_ >= 0.255450) passVetoId_check = 0;
-      if(full5x5_sigmaIetaIeta_ >= 0.031849) passVetoId_check = 0;
-      if(hOverE_ >= 0.223870) passVetoId_check = 0;
-      if(d0_ >= 0.342293) passVetoId_check = 0;
-      if(dz_ >= 0.953461) passVetoId_check = 0;
-      if(ooEmooP_ >= 0.155501) passVetoId_check = 0;
-      if(relIsoWithEA >= 0.177032) passVetoId_check = 0;
-      if(expectedMissingInnerHits_>3) passVetoId_check = 0;
-      if(!passConversionVeto_) passVetoId_check = 0;
+      if(dEtaIn_ >= 0.011932) passVetoId_cuts = 0;
+      if(dPhiIn_ >= 0.255450) passVetoId_cuts = 0;
+      if(full5x5_sigmaIetaIeta_ >= 0.031849) passVetoId_cuts = 0;
+      if(hOverE_ >= 0.223870) passVetoId_cuts = 0;
+      if(d0_ >= 0.342293) passVetoId_cuts = 0;
+      if(dz_ >= 0.953461) passVetoId_cuts = 0;
+      if(ooEmooP_ >= 0.155501) passVetoId_cuts = 0;
+      if(relIsoWithEA >= 0.177032) passVetoId_cuts = 0;
+      if(expectedMissingInnerHits_>3) passVetoId_cuts = 0;
+      if(!passConversionVeto_) passVetoId_cuts = 0;
     }
 
-    if(passVetoId_==0 && passVetoId_check==1) trueiszero++;
-    if(passVetoId_==1 && passVetoId_check==0) trueisone++;
+    if(passVetoId_==0 && passVetoId_cuts==1) IDiszero++;
+    if(passVetoId_==1 && passVetoId_cuts==0) IDisone++;
 
-    if(passVetoId_==passVetoId_check) {
-      hrelIsoWithEA_agree->Fill(relIsoWithEA);
-      hdEtaIn_agree->Fill(dEtaIn_);
-      hdPhiIn_agree->Fill(dPhiIn_);
-      hsieie_agree->Fill(full5x5_sigmaIetaIeta_);
-      hhOverE_agree->Fill(hOverE_);
-      hd0_agree->Fill(d0_);
-      hdz_agree->Fill(dz_);
-      hooEmooP_agree->Fill(ooEmooP_);
-      hmissingHits_agree->Fill(expectedMissingInnerHits_);
-      hconversion_agree->Fill(passConversionVeto_);
-    } else if(passVetoId_==0 && passVetoId_check==1) {
-      hrelIsoWithEA->Fill(relIsoWithEA);
-      hdEtaIn->Fill(dEtaIn_);
-      hdPhiIn->Fill(dPhiIn_);
-      hsieie->Fill(full5x5_sigmaIetaIeta_);
-      hhOverE->Fill(hOverE_);
-      hd0->Fill(d0_);
-      hdz->Fill(dz_);
-      hooEmooP->Fill(ooEmooP_);
-      hmissingHits->Fill(expectedMissingInnerHits_);
-      hconversion->Fill(passConversionVeto_);
-    }
-
-    prof->Fill(pt_, passVetoId_);
-    prof_check->Fill(pt_,passVetoId_check);
+    profID_pt->Fill(pt_, passVetoId_);
+    profID_eta->Fill(etaSC_, passVetoId_);
+    profCuts_pt->Fill(pt_,passVetoId_cuts);
+    profCuts_eta->Fill(etaSC_,passVetoId_cuts);
 
     if (passVetoId_) p++;
   }
 
-  // scale
-  hrelIsoWithEA->Scale(1/hrelIsoWithEA->Integral());
-  hrelIsoWithEA_agree->Scale(1/hrelIsoWithEA_agree->Integral());
-  hdEtaIn->Scale(1/hdEtaIn->Integral());
-  hdEtaIn_agree->Scale(1/hdEtaIn_agree->Integral());
-  hdPhiIn->Scale(1/hdPhiIn->Integral());
-  hdPhiIn_agree->Scale(1/hdPhiIn_agree->Integral());
-  hsieie->Scale(1/hsieie->Integral());
-  hsieie_agree->Scale(1/hsieie_agree->Integral());
-  hhOverE->Scale(1/hhOverE->Integral());
-  hhOverE_agree->Scale(1/hhOverE_agree->Integral());
-  hd0->Scale(1/hd0->Integral());
-  hd0_agree->Scale(1/hd0_agree->Integral());
-  hdz->Scale(1/hdz->Integral());
-  hdz_agree->Scale(1/hdz_agree->Integral());
-  hooEmooP->Scale(1/hooEmooP->Integral());
-  hooEmooP_agree->Scale(1/hooEmooP_agree->Integral());
-  hmissingHits->Scale(1/hmissingHits->Integral());
-  hmissingHits_agree->Scale(1/hmissingHits_agree->Integral());
-  hconversion->Scale(1/hconversion->Integral());
-  hconversion_agree->Scale(1/hconversion_agree->Integral());
-  
   cout << ((double)p)/electronTree_->GetEntries() << endl;
   cout << "total electrons = " << electronTree_->GetEntries() << endl;
-  cout << "trueiszero = " << trueiszero << " ---> " << trueiszero/((double)electronTree_->GetEntries()) << endl;
-  cout << "trueisone = " << trueisone << " ---> " << trueisone/((double)electronTree_->GetEntries()) << endl;
-/*
-  TCanvas* cprof = new TCanvas();
-  prof->Draw(); prof_check->Draw("same"); gPad->BuildLegend();
-  cprof->Print("cprof.png");
+  cout << "IDiszero = " << IDiszero << " ---> " << IDiszero/((double)electronTree_->GetEntries()) << endl;
+  cout << "IDisone = " << IDisone << " ---> " << IDisone/((double)electronTree_->GetEntries()) << endl;
 
-  TCanvas* crelIsoWithEA = new TCanvas();
-  hrelIsoWithEA->Draw();  hrelIsoWithEA->SetLineColor(kRed); hrelIsoWithEA_agree->Draw("same");
-  crelIsoWithEA->SetLogy(); crelIsoWithEA->Print("crelIsoWithEA.png");
+  TCanvas* cprofID_pt = new TCanvas("cprofID_pt","cprofID_pt");
+  profID_pt->SetLineColor(kRed); profCuts_pt->SetLineColor(kBlue);
+  profID_pt->GetXaxis()->SetTitle("pT");
+  profID_pt->GetYaxis()->SetTitle("Efficiency");
+  profID_pt->Draw(); profCuts_pt->Draw("same"); gPad->BuildLegend();
+  cprofID_pt->Print("cprofID_pt.png");
 
-  TCanvas* cdEtaIn = new TCanvas();
-  hdEtaIn_agree->Draw(); hdEtaIn->Draw("same"); gPad->BuildLegend();
-  cdEtaIn->SetLogy(); cdEtaIn->Print("cdEtaIn.png");
+  TCanvas* cprofID_eta = new TCanvas("cprofID_eta","cprofID_eta");
+  profID_eta->SetLineColor(kRed); profCuts_eta->SetLineColor(kBlue);
+  profID_eta->GetXaxis()->SetTitle("eta");
+  profID_eta->GetYaxis()->SetTitle("Efficiency");
+  profID_eta->Draw(); profCuts_eta->Draw("same"); gPad->BuildLegend();
+  cprofID_eta->Print("cprofID_eta.png");
 
-  TCanvas* cdPhiIn = new TCanvas();
-  hdPhiIn_agree->Draw(); hdPhiIn->Draw("same"); gPad->BuildLegend();
-  cdPhiIn->SetLogy(); cdPhiIn->Print("cdPhiIn.png");
-
-  TCanvas* csieie = new TCanvas();
-  hsieie_agree->Draw(); hsieie->Draw("same"); gPad->BuildLegend();
-  csieie->SetLogy(); csieie->Print("csieie.png");
-
-  TCanvas* chOverE = new TCanvas();
-  hhOverE_agree->Draw(); hhOverE->Draw("same"); gPad->BuildLegend();
-  chOverE->SetLogy(); chOverE->Print("chOverE.png");
-
-  TCanvas* cd0 = new TCanvas();
-  hd0_agree->Draw(); hd0->Draw("same"); gPad->BuildLegend();
-  cd0->SetLogy(); cd0->Print("cd0.png");
-
-  TCanvas* cdz = new TCanvas();
-  hdz_agree->Draw(); hdz->Draw("same"); gPad->BuildLegend();
-  cdz->SetLogy(); cdz->Print("cdz.png");
-
-  TCanvas* cooEmooP = new TCanvas();
-  hooEmooP_agree->Draw(); hooEmooP->Draw("same"); gPad->BuildLegend();
-  cooEmooP->SetLogy(); cooEmooP->Print("cooEmooP.png");
-
-  TCanvas* cmissingHits = new TCanvas();
-  hmissingHits_agree->Draw(); hmissingHits->Draw("same"); gPad->BuildLegend();
-  cmissingHits->SetLogy(); cmissingHits->Print("cmissingHits.png");
-
-  TCanvas* cconversion = new TCanvas();
-  hconversion_agree->Draw(); hconversion->Draw("same"); gPad->BuildLegend();
-  cconversion->SetLogy(); cconversion->Print("cconversion.png");
-*/
 }
